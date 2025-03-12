10348b8880e18f76c846a2782fcdbb815e28a666
vaishnavirshah <vaishnavirahulshah@gmail.com>
2025-03-12 18:15:12 +0000

    adding initial coir-main w/ debugger statements

---
+++ coir-main/coir/beir/retrieval/search/base.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+from typing import Dict
+
+class BaseSearch(ABC):
+
+    @abstractmethod
+    def search(self, 
+               corpus: Dict[str, Dict[str, str]], 
+               queries: Dict[str, str], 
+               top_k: int, 
+               **kwargs) -> Dict[str, Dict[str, float]]:
+        print('in base search')
+        pass
---
+++ coir-main/coir/evaluation.py
@@ -0,0 +1,56 @@
+import os
+import json
+import logging
+from coir.beir.retrieval.evaluation import EvaluateRetrieval
+from coir.beir.retrieval.search.dense import DenseRetrievalExactSearch as DRES
+
+
+logger = logging.getLogger(__name__)
+
+
+class COIR:
+    def __init__(self, tasks, batch_size):
+        self.tasks = tasks
+        self.batch_size = batch_size
+
+    def run(self, model, output_folder: str):
+        results = {}
+        for task_name, task_data in self.tasks.items():
+            output_file = os.path.join(output_folder, f"{task_name}.json")
+
+            # Check if the output file already exists
+            if os.path.exists(output_file):
+                print(f"Results for {task_name} already exist. Skipping task.")
+                continue
+
+            corpus, queries, qrels = task_data
+
+            # Initialize custom model
+            print('in evaluation.py: loading up dres\n')
+            custom_model = DRES(model, batch_size=self.batch_size)
+            print('in evaluation.py: loading up retriever\n')
+            retriever = EvaluateRetrieval(custom_model, score_function="cos_sim")
+
+            # Retrieve results
+            print('in evaluation.py: retrieving\n')
+            task_results = retriever.retrieve(corpus, queries)
+
+            # Evaluate results
+            print('in evaluation.py: evaluating\n')
+            ndcg, map, recall, precision = retriever.evaluate(qrels, task_results, retriever.k_values)
+            metrics = {
+                "NDCG": ndcg,
+                "MAP": map,
+                "Recall": recall,
+                "Precision": precision
+            }
+
+            # Save results
+            os.makedirs(output_folder, exist_ok=True)
+            with open(output_file, 'w') as json_file:
+                json.dump({"metrics": metrics}, json_file, indent=4)
+
+            logger.info(f"Results for {task_name} saved to {output_folder}")
+            results[task_name] = metrics
+
+        return results
